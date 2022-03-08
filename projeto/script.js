3ab2961e1f27b145b1d5cf5836538a6578ece6d5
barbosdiego <diego.gutemberg@hotmail.com>
2022-03-08 14:32:34 -0300

    js animated scroll

--- projeto/script.js
+++ projeto/script.js
@@ -66,3 +66,26 @@ function initScroll() {
 }
 
 initScroll();
+
+function initAnimaScroll() {
+  const section = document.querySelectorAll(".js-scroll");
+
+  if (section.length) {
+    const windowHalf = Math.round(window.innerHeight * 0.6);
+
+    function animaScroll() {
+      section.forEach((item) => {
+        const top = item.getBoundingClientRect().top - windowHalf;
+        // console.log(top);
+        if (top < 0) {
+          item.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimaScroll();
