8e67ad564fec0b74ab63bf81b7d882969c8c6bba
barbosdiego <diego.gutemberg@hotmail.com>
2022-03-08 11:48:38 -0300

    js scroll

--- projeto/script.js
+++ projeto/script.js
@@ -40,3 +40,29 @@ function initAccordeonNav() {
 }
 
 initAccordeonNav();
+
+function initScroll() {
+  const menu = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollSuave(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // alternative form
+    // const sectionTop = section.offsetTop
+    // window.scrollTo({
+    //   top: sectionTop,
+    //   behavior: 'smooth',
+    // })
+  }
+
+  menu.forEach((link) => link.addEventListener("click", scrollSuave));
+}
+
+initScroll();
