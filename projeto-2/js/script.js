ea75b068ed687960899b1c25275cee39c50d8baa
barbosdiego <diego.gutemberg@hotmail.com>
2022-03-11 14:30:27 -0300

    update js name files

--- projeto-2/js/script.js
+++ projeto-2/js/script.js
@@ -1,6 +1,6 @@
 import iniTabNav from "./tabNav.js";
-import initScroll from "./scrollSuave.js";
-import initAnimaScroll from "./animaScroll.js";
+import initScroll from "./scroll-suave.js";
+import initAnimaScroll from "./anima-scroll.js";
 import initAccordeonNav from "./accordeon.js";
 import initModal from "./modal.js"
 
