7ef745361026b12082917bfe0815e01c08496fde
barbosdiego <diego.gutemberg@hotmail.com>
2022-03-14 14:37:49 -0300

    menu mobile

--- projeto-2/js/script.js
+++ projeto-2/js/script.js
@@ -4,7 +4,8 @@ import initAnimaScroll from "./anima-scroll.js";
 import initAccordeonNav from "./accordeon.js";
 import initModal from "./modal.js";
 import initTooltip from "./tooltip.js";
-import iniDropdown from "./dropdown-menu.js"
+import iniDropdown from "./dropdown-menu.js";
+import initMenuMobile from "./menu-mobile.js";
 
 iniTabNav();
 initScroll();
@@ -13,3 +14,4 @@ initAccordeonNav();
 initModal();
 initTooltip();
 iniDropdown();
+initMenuMobile();
