61131f00b976355f19c1371380b6c6e8e2a17383
barbosdiego <diego.gutemberg@hotmail.com>
2022-03-14 09:19:52 -0300

    click outside

--- projeto-2/js/dropdown-menu.js
+++ projeto-2/js/dropdown-menu.js
@@ -1,15 +1,19 @@
-export default function iniDropdown() {}
-
-const links = document.querySelectorAll("[data-dropdown]");
-
-links.forEach((link) => {
-  ["touchstart", "click"].forEach((userEvent) => {
-    link.addEventListener(userEvent, handleClick);
+import outsideClick from "./outside-click.js";
+export default function iniDropdown() {
+  const links = document.querySelectorAll("[data-dropdown]");
+  
+  links.forEach((link) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      link.addEventListener(userEvent, handleClick);
+    });
   });
-});
-
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle("ativo");
-  console.log(this);
+  
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle("ativo");
+    outsideClick(this, ["touchstart", "click"], () => {
+      this.classList.remove("ativo");
+    });
+  }
 }
+
