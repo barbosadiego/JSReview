f84a28452cf22d7bb4ea6e734b3e953452cb9909
barbosadiego <diego.gutemberg@hotmail.com>
2022-03-18 15:04:59 -0300

    refatoracao accordeon

--- projeto-2/js/accordeon.js
+++ projeto-2/js/accordeon.js
@@ -1,19 +1,27 @@
-export default function initAccordeonNav() {
-  const accordeonList = document.querySelectorAll("[data-js='accordeon'] dt");
-  const activeClass = "ativo";
+export default class AccordeonNav {
+  constructor(list){
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
+  }
 
-  if (accordeonList.length) {
-    accordeonList[0].classList.add(activeClass);
-    accordeonList[0].nextElementSibling.classList.add(activeClass);
-
-    function activeAccordeon() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordeonList.forEach((item) => {
-      item.addEventListener("click", activeAccordeon);
+  toggleAccordeon(item){
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+  // adiciona eventos ao accordeon
+  addAccordionEvent(){
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordeon(item));
     });
+  }
+  // iniciar função
+  init(){
+    if (this.accordionList.length) {
+      // ativar primeiro item
+      this.toggleAccordeon(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+    return this
   }
 }
 
