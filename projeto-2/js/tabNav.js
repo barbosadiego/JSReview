cf5d240cbcb6fd025da8cf6b1cb20372644b99db
barbosadiego <diego.gutemberg@hotmail.com>
2022-03-18 15:37:51 -0300

    refatoracao tabnav

--- projeto-2/js/tabNav.js
+++ projeto-2/js/tabNav.js
@@ -1,20 +1,25 @@
-export default function iniTabNav() {
-  const tabMenu = document.querySelectorAll("[data-js='tab-menu'] li");
-  const tabContent = document.querySelectorAll("[data-js='tab-content'] section");
-
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("show-down");
-
-    function activeTab(index) {
-      tabContent.forEach((item) => item.classList.remove(item.dataset.anime));
-      tabContent[index].classList.add(tabContent[index].dataset.anime);
-    }
-
-    tabMenu.forEach((item, index) => {
-      item.addEventListener("click", () => {
-        activeTab(index);
-      });
+export default class TabNav {
+  constructor(menu, content) {
+    this.menu = document.querySelectorAll(menu);
+    this.content = document.querySelectorAll(content);
+  }
+  // ativa a tab de acordo com o index
+  activeTab(index) {
+    this.content.forEach((item) => item.classList.remove(item.dataset.anime));
+    this.content[index].classList.add(this.content[index].dataset.anime);
+  }
+  // adiciona os eventos nas tabs
+  addTabEvent() {
+    this.menu.forEach((item, index) => {
+      item.addEventListener("click", () => this.activeTab(index));
     });
   }
-  // console.log('oi')
+
+  init() {
+    if (this.menu.length && this.content.length) {
+      // ativar primeiro item
+      this.activeTab(0);
+      this.addTabEvent();
+    }
+  }
 }
