40a32aa4fbb68f1b7119516f4cc3c36f27aa44cc
barbosadiego <diego.gutemberg@hotmail.com>
2022-03-21 14:49:56 -0300

    refatoracao-dropdown-menu

--- projeto-2/js/dropdown-menu.js
+++ projeto-2/js/dropdown-menu.js
@@ -1,19 +1,37 @@
 import outsideClick from "./outside-click.js";
-export default function iniDropdown() {
-  const links = document.querySelectorAll("[data-dropdown]");
-  
-  links.forEach((link) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      link.addEventListener(userEvent, handleClick);
-    });
-  });
-  
-  function handleClick(event) {
-    event.preventDefault();
-    this.classList.toggle("ativo");
-    outsideClick(this, ["touchstart", "click"], () => {
-      this.classList.remove("ativo");
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+    this.activeCLass = "ativo";
+    if (events === undefined) {
+      this.events = ["touchstart", "click"];
+    } else {
+      this.events = events;
+    }
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((link) => {
+      this.events.forEach((userEvent) => {
+        link.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
   }
-}
 
+  activeDropdownMenu(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    element.classList.toggle(this.activeCLass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeCLass);
+    });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
+  }
+}
